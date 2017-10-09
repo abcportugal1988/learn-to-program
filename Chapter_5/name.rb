18c5f91ee2f4902479057f90b51c2f867b0c3896
antonio <antoniobelmardacosta@gmail.com>
2017-10-09 13:00:17 +0100

    chapter 5 exercises

---
+++ Chapter_5/name.rb
@@ -0,0 +1,4 @@
+puts "Hello there! What\'s your name?"
+name=gets.chomp
+puts "Your name is #{name}? What a lovely name!"
+puts "Pleased to meet you "+name
---
+++ Chapter_5/number.rb
@@ -0,0 +1,3 @@
+puts "What is your favourite number?"
+answer=gets.chomp
+puts "Hmmm isn't "+(answer.to_i+1).to_s+" a better number?"
