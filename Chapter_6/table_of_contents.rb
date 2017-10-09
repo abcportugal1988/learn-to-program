07d0532a0fe59f03d77cadb6a33aa470e69b3075
antonio <antoniobelmardacosta@gmail.com>
2017-10-09 17:15:59 +0100

    Chapter 6 - first exercises

---
+++ Chapter_6/table_of_contents.rb
@@ -0,0 +1,6 @@
+spacing=100
+puts ("Table of Contents").center(spacing)
+puts ""
+puts "Chapter 1: Getting Started".ljust(spacing/2)+"page 1".rjust(spacing/2)
+puts "Chapter 1: Numbers".ljust(spacing/2)+"page 9".rjust(spacing/2)
+puts "Chapter 1: Letters".ljust(spacing/2)+"page 13".rjust(spacing/2)
