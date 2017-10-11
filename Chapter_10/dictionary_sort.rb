84ca54b4f7aa2ce7082f53d5c22d2def3c2a24f9
antonio <antoniobelmardacosta@gmail.com>
2017-10-11 14:50:42 +0100

    Chapter_10

---
+++ Chapter_10/dictionary_sort.rb
@@ -0,0 +1,20 @@
+def alphabetize(unsorted,sorted)
+  if unsorted.length<=0
+    return sorted
+  else
+    smallest=unsorted.pop
+    still_unsorted=[]
+    unsorted.each do |x|
+      if x.downcase<smallest.downcase
+        still_unsorted.push(smallest)
+        smallest=x
+      else
+        still_unsorted.push(x)
+      end
+    end
+    sorted.push(smallest)
+    alphabetize(still_unsorted,sorted)
+  end
+end
+
+puts alphabetize(["a", "C", "D", "b"],[])
